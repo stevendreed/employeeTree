c09256db9a811ea96f5fae011586805b658cbadb
stevendreed <steven.daniel.reed@gmail.com>
2024-01-06 11:59:58 -0800

    created mySQL tables in schema which initializes empty relationships

---
+++ model/db/schema.sql
@@ -0,0 +1,36 @@
+-- warning: schema resets the model: run to initialize the db ONLY --
+DROP DATABASE IF EXISTS company_tree;
+
+-- initialize DB -- 
+CREATE DATABASE company_tree;
+
+USE company_tree;
+	CREATE TABLE department(
+		id int NOT NULL,
+        name VARCHAR(30) NOT NULL UNIQUE,
+        PRIMARY KEY (id)
+	);
+    CREATE TABLE role(
+		id int NOT NULL,
+        title VARCHAR(30) NOT NULL,
+        salary DECIMAL,
+        department_id INT,
+        PRIMARY KEY (id),
+        FOREIGN KEY (department_id)
+			REFERENCES department(id)
+            ON DELETE SET NULL
+    );
+    CREATE TABLE employee(
+		id int NOT NULL,
+        first_name VARCHAR(30),
+        last_name VARCHAR(30),
+        role_id INT,
+        manager_id INT,
+        PRIMARY KEY (id),
+        FOREIGN KEY (role_id)
+			REFERENCES role(id)
+            ON DELETE SET NULL,
+		FOREIGN KEY (manager_id)
+			REFERENCES employee(id)
+            ON DELETE SET NULL
+    );
