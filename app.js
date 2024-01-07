03dccb9f27972dc3b608dbacb0745b59c607e7a8
stevendreed <steven.daniel.reed@gmail.com>
2024-01-07 15:23:56 -0800

    recursively implemented mainMenu() to loop through options until user is finished

--- app.js
+++ app.js
@@ -25,21 +25,66 @@ const questions = [
             'DELETE A role (caution!)',
             'DELETE AN employee (caution!)'
         ]
+    },
+    {
+        'type': 'confirm',
+        'name': 'continue',
     }
 ];
 
-const app = function()
+const mainMenu = async function()
 {
-    inquirer
+    return inquirer
         .prompt(questions)
         .then(answers =>
             {
-                console.log('\nSTART ANSWERS:\n',{...answers},'\nEND ANSWERS');
+                console.log('\nSTART ANSWERS:\n',
+                    {...answers},
+                    '\nEND ANSWERS');
+                /*
+                   RECURSIVE FUNCTION
+                   this enables us to loop through the questions until the user
+                   confirms they're finished with selections. Enables mass-
+                   editing, and inquirer _should_ force a y/n answer which
+                   avoids potential problem of an infinite loop w/o hitting our
+                   base case
+                */
+                if (answers.continue)
+                {
+                    return mainMenu();
+                }
+                else
+                {
+                    return answers;
+                }
             })
         .catch(err =>
             {
                 console.log(`\nSTART ERROR:\n`,`${err}`,`\nEND ERROR`);
             });
+}
+
+const app = async function()
+{
+    // let resp = await mainMenu();
+
+    // console.log(resp);
+
+    // do
+    // {
+    //     inquirer
+    //     .prompt(questions)
+    //     .then(answers =>
+    //         {
+    //             console.log('\nSTART ANSWERS:\n',{...answers},'\nEND ANSWERS');
+    //         })
+    //     .catch(err =>
+    //         {
+    //             console.log(`\nSTART ERROR:\n`,`${err}`,`\nEND ERROR`);
+    //         });
+    // } while (answers.continue === 'y' );
+
+    mainMenu();
 }
 
 
